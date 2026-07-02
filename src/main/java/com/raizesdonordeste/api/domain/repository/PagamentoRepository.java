56632519e9bc043936be405a1d66eb5112ca2159
Kleber Leite <kleberleite2020@gmail.com>
2026-07-01 22:12:06 -0300

    criando repositorios

---
+++ src/main/java/com/raizesdonordeste/api/domain/repository/PagamentoRepository.java
@@ -0,0 +1,8 @@
+package com.raizesdonordeste.api.domain.repository;
+
+import com.raizesdonordeste.api.domain.entity.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+
+}

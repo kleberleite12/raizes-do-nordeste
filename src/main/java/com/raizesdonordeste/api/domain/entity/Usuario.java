cde08c931d2f44bae203d25c0194c067f123bfe4
Kleber Leite <kleberleite2020@gmail.com>
2026-07-01 23:16:07 -0300

    adicionando getters e setters nas entidades e configurando security

--- src/main/java/com/raizesdonordeste/api/domain/entity/Usuario.java
+++ src/main/java/com/raizesdonordeste/api/domain/entity/Usuario.java
@@ -14,4 +14,18 @@ public class Usuario {
     private String senha;
     private String perfil;
 
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public String getPerfil() { return perfil; }
+    public void setPerfil(String perfil) { this.perfil = perfil; }
 }
